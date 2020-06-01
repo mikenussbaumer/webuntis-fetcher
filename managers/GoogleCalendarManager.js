2a4148e425b5bbaeb7ca32dc87d5e717aebba184
Mike Nussbaumer <info@mike-nussbaumer.com>
2020-06-01 11:59:46 +0200

    removed listEvents function

--- managers/GoogleCalendarManager.js
+++ managers/GoogleCalendarManager.js
@@ -57,33 +57,6 @@ function getAccessToken(oAuth2Client, callback) {
 }
 
 /**
- * Lists the next 10 events on the user's primary calendar.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listEvents(auth) {
-    const calendar = google.calendar({ version: 'v3', auth });
-    calendar.events.list({
-        calendarId: '31shl7f1me1k4ebdhic1699voc@group.calendar.google.com',
-        timeMin: (new Date()).toISOString(),
-        maxResults: 20,
-        singleEvents: true,
-        orderBy: 'startTime',
-    }, (err, res) => {
-        if (err) return console.log('The API returned an error: ' + err);
-        const events = res.data.items;
-        if (events.length) {
-            console.log('Upcoming 10 events:');
-            events.map((event, i) => {
-                const start = event.start.dateTime || event.start.date;
-                console.log(`${start} - ${event.summary}`);
-            });
-        } else {
-            console.log('No upcoming events found.');
-        }
-    });
-}
-
-/**
  * Clears the primary calendar of the user
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
